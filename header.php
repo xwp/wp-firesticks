c36862d99e7b62e816e00eda069fd1bcf6e8823c
Rob Stinson <rob@Robs-MacBook-Pro-2.local>
2017-06-20 15:08:24 +1000

    Add styling for menu for mobile view.

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'firesticks' ); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'firesticks' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 			</nav><!-- #site-navigation -->
 		</div>
